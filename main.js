0b50e0cc0f66ab5b0e55c2a33c2388f7b0a3e071
Austinmoore1492 <37962463+Austinmoore1492@users.noreply.github.com>
2019-07-02 14:21:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const mealBtn = document.getElementById("mealBtn");
 const currentMeal = document.getElementById("current-meal");
 const message = document.getElementById("message");
 const mealIdeaBtn = document.getElementById("mealIdeaBtn");
-let menuArray = JSON.parse(localStorage.getItem('menu')) || [];
+
 
 const menu = [
   "Potato soup",
@@ -29,10 +29,11 @@ function showMeal(menuArray) {
   currentMeal.innerHTML = menuArray[randIndex];
   message.innerHTML = "How about this?";
   message.style.color = "#003b6f";
-
+  console.log(localStorage);
 }
 
 
+//function to add users menu items
 mealIdeaBtn.addEventListener("click", () => pushMeal(menu));
 
 function pushMeal() {
@@ -43,12 +44,20 @@ function pushMeal() {
   } else if(mealIdea.value === "undefined") {
     message.innerHTML = "Something Went Wrong";
   } else {    
-   localStorage.setItem('menu', JSON.stringify(menu));
-   menu.push(mealIdea.value);
-  localStorage.setItem('menuArray', JSON.stringify(menu));
     
+   store();
    message.innerHTML = `Added ${mealIdea.value}`;
    message.style.color = "#003b6f";
    mealIdea.value = "";
   }
+}
+
+//function to store values into menu array
+function store(){
+   let menuArray = JSON.parse(localStorage.getItem('menu')) || [];
+   localStorage.setItem('menuArray', JSON.stringify(menu)); 
+   menu.push(mealIdea.value);
+   localStorage.setItem('menu', JSON.stringify(menu));
+  
+   console.log(localStorage);
 }
